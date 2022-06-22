e15968d43d047a737caaa10383f6140fb68ff489
FilipLabudax <107951730+FilipLabudax@users.noreply.github.com>
2022-06-22 17:19:27 +0200

    Update Main.java

--- projekt/Main.java
+++ projekt/Main.java
@@ -1,22 +1,13 @@
 package com.company;
-
-public class Main extends Sudoku{
+public class Main extends Sudoku {
     public Main(int[][] plansza) {
         super(plansza);
     }
 
     public static void main(String[] args) {
-        Sudoku sudoku = new Sudoku(PLANSZA1);
-        System.out.println("Plansza do rozwiazania:");
-        sudoku.wstaw();
-        sudoku.wyswietl();
+        Sudoku sudoku = new Sudoku(org_plansza);
+        Sudoku.ustawienia();
 
-        // rozwiazywanie planszy sudoku
-        if (sudoku.solve()) {
-            System.out.println("Rozwiazanie Sudoku");
-            sudoku.wyswietl();
-        } else {
-            System.out.println("Nie można rozwiazać");
-        }
+
     }
 }
